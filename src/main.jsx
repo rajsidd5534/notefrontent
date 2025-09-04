931f7dd57d4930b4d6e9a3ba80a56f12290ea0be
Md Raj Siddiqui <rajsidd5534@gmail.com>
2025-09-04 17:56:09 +0530

    some change again

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./style.css";
 
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-
-// Initialize Sentry
-Sentry.init({
-  dsn: "https://abcd1234@o0.ingest.sentry.io/0", // your real DSN
-  integrations: [new BrowserTracing()],
-  tracesSampleRate: 0.1,
-});
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Sentry.ErrorBoundary fallback={"An error has occurred"}>
-        <App />
-      </Sentry.ErrorBoundary>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
