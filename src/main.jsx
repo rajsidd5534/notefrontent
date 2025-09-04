b311dc34c365f5f07ec0713d9901c3d48548515b
Md Raj Siddiqui <rajsidd5534@gmail.com>
2025-09-04 19:53:35 +0530

    some change again

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./style.css";
 import "./sentry";
+import "./style.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
