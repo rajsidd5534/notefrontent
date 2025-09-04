c973694b98aaccfbf7a7e4ea09d0abbe5a34cbd0
Md Raj Siddiqui <rajsidd5534@gmail.com>
2025-09-04 20:06:21 +0530

    change

--- src/sentry.js
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [new BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
